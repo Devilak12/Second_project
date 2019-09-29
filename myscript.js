f4ad280584e5612faacfa8b8dd568e9b03170ad3
Devilak12 <mrakhi432@gmail.com>
2019-09-29 21:09:02 +0530

    final_comit

--- myscript.js
+++ myscript.js
@@ -42,4 +42,19 @@ function motion(){
         lt=0;
       }
 }
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
 
