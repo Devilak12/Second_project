b858cb68630aaf18f85bda42062e7107d8a0e19c
Devilak12 <mrakhi432@gmail.com>
2019-10-02 10:36:19 +0530

    site_complete

--- myscript.js
+++ myscript.js
@@ -35,10 +35,20 @@ function motion(){
       if(lt==0)
       {
         document.getElementById('ham_index').style.display="flex";
+        document.getElementById('sp1').style.top='50%';
+        document.getElementById('sp1').style.transform='rotate(45deg)';
+        document.getElementById('sp2').style.display='none';
+        document.getElementById('sp3').style.transform='rotate(-45deg)';
+        document.getElementById('sp3').style.bottom='50%';
         lt=1;
       }
       else{
         document.getElementById('ham_index').style.display="none";
+        document.getElementById('sp1').style.transform='rotate(0deg)';
+        document.getElementById('sp1').style.top='20%';
+        document.getElementById('sp2').style.display='block';
+        document.getElementById('sp3').style.transform='rotate(0deg)';
+        document.getElementById('sp3').style.bottom='10%';
         lt=0;
       }
 }
